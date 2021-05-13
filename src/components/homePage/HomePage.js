391a71532a5487ce5a657275f6e76505b2218450
Jake Grella <jgrella13@gmail.com>
2021-05-13 08:35:28 -0500

    update home content

--- src/components/homePage/HomePage.js
+++ src/components/homePage/HomePage.js
@@ -1,8 +1,5 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 import StyledHomePage from './HomePageStyles';
-// import ProjectCard from '../projectCard/ProjectCard';
-// import lanMock from '../../assets/lan-mock.png';
 
 export default function HomePage() {
   return (
@@ -11,10 +8,10 @@ export default function HomePage() {
         <h3>bio</h3>
         <p>
           Hey and thanks for visiting my small corner of the internet. I'm a
-          software developer with a passion for good product design and
-          business. I enjoy brainstorming new ideas and then building them. I
-          love working on projects that help people, and if you or your company
-          does that then I'd love to talk.
+          software developer with a dedication to good product design and an
+          interst in business. I enjoy brainstorming new ideas and then building
+          them. I love working on projects that help people, and if you or your
+          company does that then I'd love to talk.
         </p>
         <br />
         <p>
@@ -43,8 +40,9 @@ export default function HomePage() {
           </li>
           <li>
             <p>
-              💡 interested in: education/ coaching and it’s effects on the
-              success of young people,{' '}
+              💡 interested in: education and coaching, it’s effects on the
+              success of young people, and how we can maximize the benefits of
+              good instruction
             </p>
           </li>
           <li>
@@ -117,58 +115,16 @@ export default function HomePage() {
               The My First Million Podcast
             </a>
           </li>
-        </ul>
-      </section>
-      {/* <br />
-      <section>
-        <h3>some of my very talented friends</h3>
-        <ul>
           <li>
             <a
-              href='https://www.robcvazquez.com/'
+              href='https://youtu.be/TGO-CAImUeY'
               target='_blank'
               rel='noreferrer'>
-              Roberto Vazquez
+              Jon Bellion - The Making Of Guillotine
             </a>
           </li>
         </ul>
-      </section> */}
-      {/* <div className='home-banner'>
-        <h2>
-          Full-stack software developer currently seeking a place to make a
-          difference.
-        </h2>
-      </div>
-      <section className='project-cards-section'>
-        <ProjectCard
-          mockImg={lanMock}
-          bgImg='https://images.unsplash.com/photo-1557682257-2f9c37a3a5f3'
-          imgAlt='lambda alumni network'
-          title='Lambda Alumni Network'
-          description='A unified, private community for Lambda School alumni to connect, advance their careers, and give back in a forum-like environment.'
-          demoLink='https://main.d37zm5ayhfot8q.amplifyapp.com/'
-          gitHubLink='https://github.com/Lambda-School-Labs/lan-fe-a'
-        />
-        <ProjectCard
-          imgURL='https://images.unsplash.com/photo-1557683325-3ba8f0df79de'
-          imgAlt='townsquare'
-          title='Townsquare'
-          description='A platform for citizens to make their voices heard on the issues they would like to see resolved in their community.'
-          demoLink='https://jakegrella.com'
-          gitHubLink='https://github.com/jakegrella'
-        />
-        <Link to='/work' className='all-projects-btn'>
-          view more work
-        </Link>
       </section>
-      <section className='get-in-touch'>
-        <a href='mailto:jake@jakegrella.com'>Get in touch.</a>
-        <p>
-          I currently live in Chicago, IL, and I am looking for remote work, but
-          I am interested in relocating if it means I am part of something
-          important or impactful.
-        </p>
-      </section> */}
     </StyledHomePage>
   );
 }
